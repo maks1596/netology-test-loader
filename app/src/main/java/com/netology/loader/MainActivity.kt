f02c9976bc86466c3e0d7e4d6b39e8c1aca90ac3
maks1596 <386125Mm>
2021-03-25 20:46:07 +0300

    Добавлен показ индикатора загрузки

--- app/src/main/java/com/netology/loader/MainActivity.kt
+++ app/src/main/java/com/netology/loader/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.netology.loader.databinding.MainBinding
 
-internal class MainActivity : AppCompatActivity(R.layout.activity_main) {
+internal class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val binding = MainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.progressIndicator.show()
+
         lifecycleScope.launchWhenResumed {
             val data = GroupRepository.getData()
             Log.d("MainActivity", data.toString())
+            binding.progressIndicator.hide()
         }
     }
 }
